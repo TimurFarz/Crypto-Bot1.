732b0a9230e6319dd7f878c97db97a08d25e614a
TimurFarz <131834664+TimurFarz@users.noreply.github.com>
2023-07-12 15:37:38 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -19,3 +19,25 @@ def values(message: telebot.types.Message):
     for key in keys.keys():
         text = '\n'.join((text, key, ))
     bot.reply_to(message, text)
+
+
+@bot.message_handler(content_types=['text', ])
+def convert(message: telebot.types.Message):
+    try:
+        values = message.text.split(' ')
+
+        if len(values) != 3:
+            raise ConvertionException('Слишком много параметров.')
+
+        quote, base, amount = values
+        total_base = CryptoConverter.convert(quote, base, amount)
+    except ConvertionException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+    except Exception as e:
+        bot.reply_to(message, f'Не удалось обработать команду.\n{e}')
+    else:
+        text = f'Цена {amount} {quote} в {base} - {total_base}'
+        bot.send_message(message.chat.id, text)
+
+
+bot.polling()
---
+++ config.py
@@ -0,0 +1,6 @@
+TOKEN = '6389660861:AAGrsISy4MxL-rCDg6UQ6tQkoACbSJZEv4Y'
+keys = {
+    'евро': 'EUR',
+    'доллар': 'USD',
+    'рубль': 'RUB',
+}
