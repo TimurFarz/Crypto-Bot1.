2937f9fb6e5139672fc86f128a42443c94c937eb
TimurFarz <131834664+TimurFarz@users.noreply.github.com>
2023-07-12 15:36:42 -0400

    app.py

---
+++ app.py
@@ -0,0 +1,21 @@
+import telebot
+from config import keys, TOKEN
+from extensions import ConvertionException, CryptoConverter
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['start', 'help'])
+def help(message: telebot.types.Message):
+    text = 'Чтобы начать работу введите команду боту в следующем формате:\n<имя валюты>\
+<в какую валюту перевести>\
+<колличество переводимой валюты>\nУвидеть список всех доступных валют: /values'
+    bot.reply_to(message, text)
+
+
+@bot.message_handler(commands=['values'])
+def values(message: telebot.types.Message):
+    text = 'Доступные валюты:'
+    for key in keys.keys():
+        text = '\n'.join((text, key, ))
+    bot.reply_to(message, text)
